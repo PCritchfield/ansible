29dcfbf311b66821489ffae79ac3088e215dfd80
phil.critchfield <phil.critchfield@contino.io>
2022-08-07 10:51:47 -0400

    fix merge conflict

--- ansible_molecule_pt2/laravel_website/laravel_role/molecule/default/tests/test_site.py
+++ ansible_molecule_pt2/laravel_website/laravel_role/molecule/default/tests/test_site.py
@@ -2,7 +2,7 @@
 
 import pytest
 import testinfra.utils.ansible_runner
-import os
+import re
 
 # testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 #     os.environ["MOLECULE_INVENTORY_FILE"]).get_hosts('site')
@@ -12,8 +12,10 @@ testinfra_hosts = ["site"]
 def test_nginx_is_installed(host):
     nginx = host.package("nginx")
     assert nginx.is_installed
+    print (nginx.version)
+    assert re.match('1.18*', nginx.version)
 
 def test_nginx_running_and_enabled(host):
      nginx = host.service("nginx")
      assert nginx.is_running
-     assert nginx.is_enabled 
+     assert nginx.is_enabled
