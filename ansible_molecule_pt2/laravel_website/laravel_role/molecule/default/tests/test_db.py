73ca64b5fbd355c739d5a198964e9b6a3f8b9808
Phil Critchfield <pjcritchfield@tuta.io>
2022-08-21 20:09:19 -0400

    Fix test_db.py

--- ansible_molecule_pt2/laravel_website/laravel_role/molecule/default/tests/test_db.py
+++ ansible_molecule_pt2/laravel_website/laravel_role/molecule/default/tests/test_db.py
@@ -2,11 +2,11 @@
 
 testinfra_hosts = ["mysql"]
 
-def test_mariadb_is_installed(host):
-    mariadb = host.package("mariadb")
-    assert mariadb.is_installed
+def test_mysql_is_installed(host):
+    mysql = host.package("mysql-server")
+    assert mysql.is_installed
 
-def test_mariadb_running_and_enabled(host):
-     mariadb = host.service("mariadb")
-     assert mariadb.is_running
-     assert mariadb.is_enabled 
+def test_mysql_running_and_enabled(host):
+     mysql = host.service("mysql")
+     assert mysql.is_running
+     assert mysql.is_enabled 
